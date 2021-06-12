f3277d1a5798d8cd67972ea57c851241dbf26071
evandr0 <evandro@live.jp>
2021-06-12 09:40:30 -0300

    adicionado novas aulas

---
+++ load.js
@@ -0,0 +1,14 @@
+function Carreguei(){
+    document.getElementById("log").innerHTML += "Carreguei<br>";
+}
+function Redimensionei(){
+    document.getElementById("log").innerHTML += "Redimensionei<br>"
+
+}
+
+/* 
+* document. referenciado a página html 
+* getEmentById = procura no doc o ID chamado log
+* innerHTML = quero colocar uma informação 
+* += adiciona o trecho seguinte. insere/adiciona a informação no ID LOG.
+*/
---
+++ meucodigo.js
@@ -0,0 +1,10 @@
+var a = 2;
+var b = 3;
+consolelog(a+b);
+
+
+
+function consolelog(a){
+    console.log("alert = " + a)
+
+}
